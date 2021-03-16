429232a30a8800923ec7ed3fca98ecfe88d1c454
GalEvenHaim <galeh1992@Gmail.com>
2021-03-16 07:52:28 +0200

    updated chrome options

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def args_parse():
     parser = argparse.ArgumentParser(
         description="A program to search for appartments in yad_2 according to given city, number of rooms and price "
                     "range")
-    parser.add_argument("c", "--city", default="Givataym", type=str, choices=["Givataym", "Tel Aviv", "Ramat Gan"],
+    parser.add_argument("city", default="Givataym", type=str, choices=["Givataym", "Tel Aviv", "Ramat Gan"],
                         help="choose between Givataym, Tel Aviv, and Ramat Gan")
     parser.add_argument("min_rooms", type=str, help="min number of rooms")
     parser.add_argument("max_rooms",  type=str, help="max number of rooms")
@@ -22,16 +22,15 @@ def args_parse():
 def main():
     args = args_parse()
     city_dict = {"Givataym": "6300", "Tel Aviv": "5000", "Ramat Gan": "8600"}
-    WORKERS = args.workers
     search_link = f"https://www.yad2.co.il/realestate/forsale?city={city_dict[args.city]}" \
                   f"&rooms={args.min_rooms}-{args.max_rooms}&price=-1-{args.max_price}&parking=1&elevator=1&squaremeter={args.min_sqr}--1"
     print(f"finding apartments in {args.city} with  {args.min_rooms}-{args.max_rooms} rooms ")
-    apartment_set = set()
+    apartments_set = set()
     item_list = get_apartments_item_index()
     print(f"found {len(item_list)} apartments.")
-    start_crawling(item_list)
-    set_to_sheet(apartment_set)
-    print(f"uploaded {len(apartment_set)} to google sheets.")
+    start_crawling(item_list, args.workers, apartments_set)
+    set_to_sheet(apartments_set)
+    print(f"uploaded {len(apartments_set)} to google sheets.")
 
 
 main()
